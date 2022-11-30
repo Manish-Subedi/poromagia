cffbeaf7c44ec039b6759f32e1ce29e8215637e2
manish <manis.subedi68@gmail.com>
2022-11-30 19:38:36 +0200

    fixed response from Arduino

--- i2cmaster.py
+++ i2cmaster.py
@@ -2,9 +2,9 @@
 # Raspberry Pi as Master and Arduino as Slave (I2C)
 # Raspi subscribes to a topic for status of the sorting machine (ON/OFF)
 # and also gets the decision (int) from the same topic
-# The Raspi then writes the integer to I2C (Arduino) 
-# Arduino performs necessary action, puts the card to the respective box. 
-# The Arduino, on the other hand, sends a command when the card is on top of the camera. 
+# The Raspi then writes the integer to I2C (Arduino)
+# Arduino performs necessary action, puts the card to the respective box.
+# The Arduino, on the other hand, sends a command when the card is on top of the camera.
 
 # i2cmaster.py
 # Author: Manish Subedi
@@ -23,7 +23,7 @@ def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
         # Subscribing in on_connect() means that if we lose  the
         # connection and reconnect, subscriptions will be renewed
-        client.subscribe("decision")
+        client.subscribe("statusChange")
     else:
         print("Connection failed with "+str(rc))
 
@@ -33,9 +33,9 @@ def on_message(client, userdata, msg):
     # cast to string
     json_raw = str(msg.payload.decode())
     json_parsed = json.loads(json_raw)
-    int = json_parsed["int"]
+    int = json_parsed["decision"]
     status = json_parsed["status"]
-    print("The integer is "+str(int)+" "+"and the status is "+str(status))
+    print("The decision is "+str(int)+" "+"and the status is "+str(status))
     # Only print the valid decision
     """
     0 means home
@@ -52,24 +52,35 @@ def on_message(client, userdata, msg):
     # Put Arduino to deep sleep if status is 0
     #if status == 0:
     #    bus.write_byte(addr, 0) # this must be configured in Arduino program as well
-"""
-
-#read from arduino 
-pos = bus.read_byte(addr, 0)
-print(pos)
-if pos == 1:
-   # take picture, [ MELANY, link to the function to take picture ]
-
-
-"""
-# Create an instance 
+     #read from arduino
 client = mqtt.Client()
-client.on_connect = on_connect
 client.on_message = on_message
-#provide login credentials
-client.username_pw_set(username="friends",password="Wow_manis")
-# Connect to the broker
+client.on_connect = on_connect
+client.username_pw_set(username="friends",password="wow_manis")
 client.connect("hetkinen.ddns.net", 2048, 60)
 client.tls_set()
-client.loop_forever()
+
+while(1):
+    take_picture = 1 #bus.read_byte(addr)
+    print(take_picture)
+
+    if take_picture == 1:
+        # take picture, [ MELANY, link to the function to take picture ]
+        client.loop_start()
+        client.on_connect
+        client.on_message
+        client.loop_stop()
+
+# Create an instance
+#client = mqtt.Client()
+#client.on_connect = on_connect
+#client.on_message = on_message
+#provide login credentials
+#client.username_pw_set(username="friends",password="Wow_manis")
+# Connect to the broker
+#client.connect("hetkinen.ddns.net", 2048, 60)
+#client.connect("test.mosquitto.org", 1883, 60)
+#client.tls_set()
+#client.loop_forever()
+
 ###### EOF ######
